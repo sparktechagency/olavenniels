56c4b14d4f75cb36c8b7987267820550bb2db4f6
hosainahmeed <skyfal430@gmail.com>
2025-09-09 10:02:46 +0600

    update

--- src/components/books/components/audio_related/AudioBookCreate.jsx
+++ src/components/books/components/audio_related/AudioBookCreate.jsx
@@ -75,13 +75,13 @@ const AudioUploadField = ({
             controls
             className="w-full h-[100px] rounded"
           />
-          <button
+          {!existingAudioUrl && <button
             onClick={handleRemoveAudio}
             className="absolute top-2 right-2 w-6 h-6 flex items-center justify-center cursor-pointer bg-white rounded-full shadow"
             aria-label="Remove audio"
           >
             <CloseOutlined className="!text-red-500" />
-          </button>
+          </button>}
         </div>
       )}
     </div>
