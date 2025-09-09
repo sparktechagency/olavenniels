4d83c6224aef850038510d0214aa8112d59c5033
hosainahmeed <skyfal430@gmail.com>
2025-09-09 16:33:25 +0600

    update

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
             }
 
             toast.success("Login successfully");
-            // window.location.href = "/";
+            window.location.href = "/";
           }
         })
         .catch((error) => {
