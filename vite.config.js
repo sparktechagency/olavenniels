d0beb127d4efd4393a809d2c83b309def720c365
hosainahmeed <skyfal430@gmail.com>
2025-09-23 14:35:19 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -2,46 +2,46 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   server: {
-//     port: 3001,
-//     host: "0.0.0.0",
-//   },
-//   preview: {
-//     port: 3001,
-//     host: "0.0.0.0",
-//   },
-// });
+// https://vite.dev/config/ *** http://13.62.91.46:3000
 export default defineConfig({
-  optimizeDeps: {
-    include: ["react", "react-dom", "antd"],
-  },
+  plugins: [react(), tailwindcss()],
   server: {
-    host: "0.0.0.0",
     port: 3001,
-    allowedHosts: ["13.49.70.96"],
+    host: "0.0.0.0",
   },
   preview: {
-    host: "0.0.0.0",
     port: 3001,
-    allowedHosts: ["13.49.70.96"],
-  },
-  plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      onwarn(warning, defaultHandler) {
-        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
-          return;
-        }
-        defaultHandler(warning);
-      },
-    },
-  },
-  esbuild: {
-    supported: {
-      "top-level-await": true,
-    },
+    host: "0.0.0.0",
   },
 });
+// export default defineConfig({
+//   optimizeDeps: {
+//     include: ["react", "react-dom", "antd"],
+//   },
+//   server: {
+//     host: "0.0.0.0",
+//     port: 3001,
+//     allowedHosts: ["13.49.70.96"],
+//   },
+//   preview: {
+//     host: "0.0.0.0",
+//     port: 3001,
+//     allowedHosts: ["13.49.70.96", "localhost"],
+//   },
+//   plugins: [react(), tailwindcss()],
+//   build: {
+//     rollupOptions: {
+//       onwarn(warning, defaultHandler) {
+//         if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+//           return;
+//         }
+//         defaultHandler(warning);
+//       },
+//     },
+//   },
+//   esbuild: {
+//     supported: {
+//       "top-level-await": true,
+//     },
+//   },
+// });
