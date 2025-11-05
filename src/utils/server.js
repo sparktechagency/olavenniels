5becc8899f606307524b1697c8a189549fc7b0d2
hosainahmeed <skyfal430@gmail.com>
2025-11-05 11:20:31 +0600

    update server api

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,7 +1,7 @@
 // export const baseUrl = "http://10.10.20.54:5600";
-export const baseUrl = "http://13.62.91.46:3000/api";
+export const baseUrl = "http://13.62.165.184:3000/api";
 // export const baseUrl = "http://192.168.0.101:3000";
-export const imageBaseUrl = "http://13.62.91.46:3000";
+export const imageBaseUrl = "http://13.62.165.184:3000";
 
 export const imageUrl = (image) => {
   return image
